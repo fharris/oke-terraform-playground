0da7ae0de615c20302c2971b133b5e2d22b2bfa6
Fernando Harris <fernando.harris@oracle.com>
2025-11-11 14:56:33 +0100

    reducing the number of BK pools for autoscaler

--- oke_addons.tf
+++ oke_addons.tf
@@ -22,8 +22,9 @@ resource "oci_containerengine_addon" "fk_oke_autoscaler_addon" {
 
     configurations {
       key = "nodes"
+      value = join("", [var.autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_autoscaler_node_pool[0].id])
       #value = join("", [var.autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_autoscaler_node_pool[0].id]," ",var.autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_autoscaler_node_pool[1].id)
-      value = join("", [var.autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_autoscaler_node_pool[0].id,",",var.backup_autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_bk_autoscaler_node_pool[0].id,",",var.backup_autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_bk_autoscaler_node_pool[1].id,",",var.backup_autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_bk_autoscaler_node_pool[2].id])
+      #value = join("", [var.autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_autoscaler_node_pool[0].id,",",var.backup_autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_bk_autoscaler_node_pool[0].id,",",var.backup_autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_bk_autoscaler_node_pool[1].id,",",var.backup_autoscaler_min_number_of_nodes,":",var.autoscaler_max_number_of_nodes,":",oci_containerengine_node_pool.fk_oke_bk_autoscaler_node_pool[2].id])
     }
 
 
